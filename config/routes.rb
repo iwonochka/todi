f6acf3a2a9dd2044890b78ece786e4ddb2ca51d2
Elena Teske <elenateske@gmx.net>
2022-03-02 11:15:41 +0100

    third

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'pages#start'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show, :new, :create, :update]
   resources :events, only: [:show, :new, :create, :index, :update]
   get "profiles/:id", to: "profiles#show"
+  get "/home", to: "pages#home"
 end
