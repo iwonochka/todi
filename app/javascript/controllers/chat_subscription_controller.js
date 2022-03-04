b9e663be971a37bb05d51b3d1c0fc36a3e5a1751
Frank Obeng <obengfan@gmail.com>
2022-03-04 15:19:31 +0100

    done with the chat

--- app/javascript/controllers/chat_subscription_controller.js
+++ app/javascript/controllers/chat_subscription_controller.js
@@ -3,9 +3,25 @@ import consumer from "../channels/consumer"
 
 export default class extends Controller {
   static values = { chatId: Number }
-  static targets = ["messages", "form"]
+  static targets = ["messages", "form", "input"]
 
   connect() {
-    console.log(`Subscribe to the chatroom with the id ${this.chatIdValue}.`)
+
+    this.channel = consumer.subscriptions.create(
+      { channel: "ChatChannel", id: this.chatIdValue },
+      { received: data => this.insertMessageScrollDownAndResetForm(data)}
+      )
+      console.log(`Subscribed to the chat with the id ${this.chatIdValue}.`)
+    }
+    insertMessageScrollDownAndResetForm(data) {
+      console.log("hello")
+      this.messagesTarget.insertAdjacentHTML("beforeend", data)
+      this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+      console.log(this.inputTarget.value)
+    this.inputTarget.value = "";
+  }
+  disconnect() {
+    console.log("Unsubscribed from the chatroom")
+    this.channel.unsubscribe()
   }
 }
